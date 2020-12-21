74a1f66c217ef6902c55cfab9f54be6e3b9ea19c
Engaz01 <azakaite@github.com>
2020-12-21 17:35:55 +0200

    Add method buildRow to FeedBackApiController

--- app/classes/Controllers/User/API/FeedbackApiController.php
+++ app/classes/Controllers/User/API/FeedbackApiController.php
@@ -20,16 +20,14 @@ class FeedbackApiController extends AuthController
 
         if ($form->validate()) {
             $user = App::$session->getUser();
-            $feedback = $form->values();
 
+            $feedback = $form->values();
             $feedback['id'] = App::$db->insertRow('feedback', $form->values() + [
                     'name' => $user['name'],
                     'timestamp' => time()
                 ]);
-            $feedback['name'] = $user['name'];
 
-            $feedback['timestamp'] = $this->timeFormat(time());
-            //TODO: feedback needs a build row
+            $feedback = $this->buildRow($user, $feedback);
             $response->setData($feedback);
         } else {
             $response->setErrors($form->getErrors());
@@ -39,6 +37,24 @@ class FeedbackApiController extends AuthController
         return $response->toJson();
     }
 
+
+    /**
+     * Formats row for json to be used in update method,
+     * so that the data would be updated in the same format.
+     *
+     * @param $user
+     * @param $feedback
+     * @return array
+     */
+    private function buildRow($user, $feedback): array
+    {
+        return $row = [
+            'id' => $feedback['id'],
+            'name' => $user['name'],
+            'comment' => $feedback['comment'],
+            'timestamp' => $this->timeFormat(time())
+        ];
+    }
 
     /**
      * Returns formatted time from timestamp given in row.
