9b22725f1e1b31e5712f01f8e7363fe272163ea7
Engaz01 <azakaite@github.com>
2020-12-22 12:49:45 +0200

    Add tabbed out code to page.tpl.php

--- core/templates/page.tpl.php
+++ core/templates/page.tpl.php
@@ -3,11 +3,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php print $data['title'] ?></title>
+
     <?php foreach ($data['css'] as $path) : ?>
+
         <link rel="stylesheet" href="<?php print $path; ?>">
+
     <?php endforeach; ?>
+
     <?php foreach ($data['js'] as $path) : ?>
+
         <script src="<?php print $path; ?>" defer></script>
+
     <?php endforeach; ?>
 
 </head>
